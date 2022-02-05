56b58efe4ab6be42ad440a472746acfbafcee23b
ALEKSANDR399 <88842048+ALEKSANDR399@users.noreply.github.com>
2022-02-05 12:54:37 +0200

    ДЗ№9

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,5 +1,6 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
+import Notiflix from 'notiflix';
 
 const areaForInputDate = document.querySelector('#datetime-picker');
 const startTimer = document.querySelector('[data-start]');
@@ -17,7 +18,7 @@ const options = {
     console.log(selectedDates[0]);
   },
 };
-
+let timerId = null;
 flatpickr(areaForInputDate, options);
 
 areaForInputDate.addEventListener('input', getDateInInput);
@@ -29,7 +30,7 @@ function getDateInInput(event) {
   const inputNumber = new Date(event.currentTarget.value);
 
   if (inputNumber.getTime() < date.getTime()) {
-    alert('Please choose a date in the future');
+    Notiflix.Notify.failure('Please choose a date in the future');
     startTimer.setAttribute('disabled', 'disabled');
   } else {
     startTimer.removeAttribute('disabled');
@@ -37,51 +38,47 @@ function getDateInInput(event) {
 }
 
 function adsStartTimer() {
-  const date = new Date(areaForInputDate.value);
-  const liveDate = options.defaultDate;
-
-  setInterval(() => {
-    convertMs(date - liveDate);
+  timerId = setInterval(() => {
+    const date = new Date(areaForInputDate.value);
+    const liveDate = new Date();
+    let different = date - liveDate.getTime();
+    convertMs(different);
     startTimer.setAttribute('disabled', 'disabled');
+    if (different < 1000) {
+      clearInterval(timerId);
+    }
   }, 1000);
 }
 
 function convertMs(ms) {
   // Number of milliseconds per unit of time
-  setInterval(() => {
-    const second = 1000;
-    const minute = second * 60;
-    const hour = minute * 60;
-    const day = hour * 24;
 
-    const days = Math.floor(ms / day);
-    // Remaining hours
-    const hours = Math.floor((ms % day) / hour);
-    // Remaining minutes
-    const minutes = Math.floor(((ms % day) % hour) / minute);
-    // Remaining seconds
-    const seconds = Math.floor((((ms % day) % hour) % minute) / second);
-    // console.log(days);
-    daysInfo.textContent = days;
-    hourInfo.textContent = hours;
-    minutesInfo.textContent = minutes;
-    secondsInfo.textContent = seconds;
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
 
-    addLeadingZero(daysInfo.textContent);
-    addLeadingZero(hourInfo.textContent);
-    addLeadingZero(minutesInfo.textContent);
-    addLeadingZero(secondsInfo.textContent);
-  }, 1000);
+  const days = addLeadingZero(Math.floor(ms / day));
+  // Remaining hours
+  const hours = addLeadingZero(Math.floor((ms % day) / hour));
+  // Remaining minutes
+  const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
+  // Remaining seconds
+  const seconds = addLeadingZero(Math.floor((((ms % day) % hour) % minute) / second));
+  // console.log(days);
+  daysInfo.textContent = days;
+  hourInfo.textContent = hours;
+  minutesInfo.textContent = minutes;
+  secondsInfo.textContent = seconds;
+
+  addLeadingZero(daysInfo.textContent);
+  addLeadingZero(hourInfo.textContent);
+  addLeadingZero(minutesInfo.textContent);
+  addLeadingZero(secondsInfo.textContent);
 
   // Remaining days
 }
 
 function addLeadingZero(value) {
-  // console.log(value);
-  if (value > 9) {
-    console.log(value);
-    value;
-  }
-  console.log(value.padStart(1, '0'));
-  value.padStart(1, '0');
+  return String(value).padStart(2, '0');
 }
